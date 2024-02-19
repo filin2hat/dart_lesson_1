93d325fd9bfc35e51801cf8e3695f62aa6b8ad22
Biryulin Dmitriy <birulin.dm@mmtr.ru>
2024-02-19 09:11:47 +0300

    the code is moved to separate functions

--- bin/dart_lesson_1.dart
+++ bin/dart_lesson_1.dart
@@ -12,17 +12,25 @@ Future<void> main(List<String> arguments) async {
       '_________________________________________________________________'
       '_________________________________________________________________';
 
-  print('\nList of Movies:\n');
-  for (var movie in movieList.getMovies()) {
-    await Future.delayed(Duration(seconds: 1));
-    print(movie);
-    print(separator);
-  }
+  await printAll(movieList, separator);
 
+  await printFiltered(movieList, separator);
+}
+
+Future<void> printFiltered(MovieList movieList, String separator) async {
   var filteredMovies = movieList.filterByYear(1972);
   print('\nFiltered Movies (Year: 1972):\n');
   for (var movie in filteredMovies) {
     await Future.delayed(Duration(seconds: 2));
+    print(movie);
+    print(separator);
+  }
+}
+
+Future<void> printAll(MovieList movieList, String separator) async {
+  print('\nList of Movies:\n');
+  for (var movie in movieList.getMovies()) {
+    await Future.delayed(Duration(seconds: 1));
     print(movie);
     print(separator);
   }
